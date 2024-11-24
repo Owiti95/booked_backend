9d1d821741ce24ebf0ac200acf4b51b147b26614
hannington owiti <hanningtonowiti@gmail.com>
2024-11-24 17:33:59 +0300

    added jwt secret key

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_secret_key')  # Change this
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')  # Change this
     
