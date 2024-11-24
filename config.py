01e0e8277a87b83cdd9b34e5b3d8718cdfc60d5d
hannington owiti <hanningtonowiti@gmail.com>
2024-11-24 17:08:38 +0300

    render url

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
 
 # Initialize the extensions globally but do not bind them to the app yet
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
+load_dotenv()
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_secret_key')  # Change this
     
